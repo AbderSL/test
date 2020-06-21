ba729ccf7a18f564d301ea54d83b32e331e5a627
AbderSL <62664228+AbderSL@users.noreply.github.com>
2020-06-21 21:51:46 +0100

    Delete tot.js

--- tot.js
+++
@@ -1,3 +0,0 @@
-function hqtvx(url){if(Hls.isSupported()) {var video = document.getElementById('hqtvx'); video.volume = 0.2; var hls = new Hls(); var hqtvxcom = decodeURIComponent(url)
-hls.loadSource(hqtvxcom); hls.attachMedia(video); hls.on(Hls.Events.MANIFEST_PARSED,function() {video.play();}); document.title = url}}
-hqtvx ("http://siyatv.herokuapp.com/https://shls-mbc3-prod.shahid.net/mbc3-prod_5.m3u8");
